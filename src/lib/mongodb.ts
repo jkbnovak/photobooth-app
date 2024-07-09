d8d5845af0097119aee253fc13a14c78485a86a0
Jakub Novák <jkbnovak@gmail.com>
2024-07-09 23:42:03 +0200

    mongo ssl

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -1,34 +1,30 @@
-import { MongoClient } from 'mongodb'
+import { MongoClient, MongoClientOptions } from 'mongodb';
 
 if (!process.env.MONGODB_URI) {
-  throw new Error('Please add your MongoDB URI to .env.local')
+  throw new Error('Please add your MongoDB URI to .env.local');
 }
 
-const uri = process.env.MONGODB_URI
-const options = {}
+const uri = process.env.MONGODB_URI;
+const options: MongoClientOptions = {
+  ssl: true,
+};
 
-let client: MongoClient
-let clientPromise: Promise<MongoClient>
+let client: MongoClient;
+let clientPromise: Promise<MongoClient>;
 
 declare global {
-  namespace NodeJS {
-    interface Global {
-      _mongoClientPromise: Promise<MongoClient>
-    }
-  }
+  var _mongoClientPromise: Promise<MongoClient>;
 }
 
 if (process.env.NODE_ENV === 'development') {
-  // In development mode, use a global variable so the MongoClient is not constantly being created and destroyed.
   if (!global._mongoClientPromise) {
-    client = new MongoClient(uri, options)
-    global._mongoClientPromise = client.connect()
+    client = new MongoClient(uri, options);
+    global._mongoClientPromise = client.connect();
   }
-  clientPromise = global._mongoClientPromise
+  clientPromise = global._mongoClientPromise;
 } else {
-  // In production mode, it's best to not use a global variable.
-  client = new MongoClient(uri, options)
-  clientPromise = client.connect()
+  client = new MongoClient(uri, options);
+  clientPromise = client.connect();
 }
 
-export default clientPromise
+export default clientPromise;
